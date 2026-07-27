733b923618044c730ae5d6e24dda7903f5eda92a
edjapps <108023691+edjapps@users.noreply.github.com>
2026-07-27 17:08:12 -0300

    Add files via upload

---
+++ docs/assets/web-z4AXGGIY.js
@@ -0,0 +1,1 @@
+import{W as e}from"./index-B32XhwwL.js";class i extends e{async requestReview(){console.warn("In-App Review is not available on web")}async getPluginVersion(){return{version:"web"}}}export{i as CapgoInAppReviewWeb};
